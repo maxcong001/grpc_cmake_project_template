76c04949af63922980f2eaf3c6a1bd10315c39f3
Max <savagecm@users.noreply.github.com>
2018-03-03 21:44:14 +0800

    aa

--- examples/example.cpp
+++ examples/example.cpp
@@ -24,10 +24,10 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 #include <iostream>
 #include "include"
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   // Instantiate the client. It requires a channel, out of which the actual RPCs
   // are created. This channel models a connection to an endpoint (in this case,
   // localhost at port 50051). We indicate that the channel isn't authenticated
